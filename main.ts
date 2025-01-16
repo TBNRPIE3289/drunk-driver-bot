71a39f914125dc6e17efbd128298d404d7e54bab
TBNRPIE3289 <168212569+TBNRPIE3289@users.noreply.github.com>
2025-01-16 11:29:12 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let sonar = 0
 cuteBot.trackEvent(cuteBot.MbPins.Left, cuteBot.MbEvents.FindLine, function () {
     cuteBot.motors(100, -100)
 })
@@ -5,21 +6,30 @@ cuteBot.trackEvent(cuteBot.MbPins.Right, cuteBot.MbEvents.FindLine, function () 
     cuteBot.motors(-100, 100)
 })
 basic.forever(function () {
-    basic.showLeds(`
-        # . # . #
-        . # # # .
-        # # # # #
-        . # # # .
-        # . # . #
-        `)
-    music.play(music.stringPlayable("C C C C C C C C ", 500), music.PlaybackMode.LoopingInBackground)
-    if (input.lightLevel() < 10) {
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 255, 255, 255)
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 255, 255)
+    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (sonar > 3 && sonar < 15) {
+        cuteBot.motors(0, 0)
+        cuteBot.motors(randint(-100, 100), randint(-100, 100))
     } else {
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+        music.play(music.stringPlayable("C C C C C C C C ", 500), music.PlaybackMode.LoopingInBackground)
+        if (input.lightLevel() < 10) {
+            cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 255, 255, 255)
+            cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 255, 255)
+        } else {
+            cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
+            cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
+        }
     }
+})
+basic.forever(function () {
+	
 })
 basic.forever(function () {
     cuteBot.forward()
